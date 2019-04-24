69bb24314fef89f1a0c2686e7e2e222758cc7959
AzezSalami <43267443+AzezSalami@users.noreply.github.com>
2019-04-24 16:37:43 +0200

    database testen

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-    
+$hostname = "51.38.112.111";                // naam van server
+$dbname = "groep35";                    // naam van database
+$gebruikersnaam = "iproject35";            // gebruikersnaam
+$pw = "iProject35";                     //wachtwoord
+
+try {
+    $dbh = new PDO ("sqlsrv:Server=$hostname;Database=$dbname;
+                ConnectionPooling=0", "$gebruikersnaam", "$pw");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    echo $e->getMessage();
+    die();
+}
+
+$query = $dbh->query('SELECT * FROM TBL_Rubriek');
+while ($r = $query->fetch()) {
+    echo $r['rubrieknummer '];
+
+}
+
+
+//echo '<pre>';
+//print_r(PDO::getAvailableDrivers());
+//echo '</pre>';
 ?>
