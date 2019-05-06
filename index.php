d879b676327dd6a0c7d1d8a2550c86535a6b961c
nielseenink <nielseenink@hotmail.com>
2019-05-06 14:25:16 +0200

    Opzet met kolommen en rijen gemaakt. Titel, Prijs, Afbeelding, Beschrijving en details knop gemaakt.

--- index.php
+++ index.php
@@ -1,44 +1,75 @@
 <head>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-	      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-	        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-	        crossorigin="anonymous"></script>
-	<style>
-		.blue {
-			background-color: blue;
-			border: aqua 1px solid;
-		}
-	</style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="CSS/homepage.css" type="text/css">
 </head>
 
 <body>
-	<main>
-		<div class="container">
-			<div class="row my-2">
-				<div class="blue col mx-2">
-					Topveiling 1
-				</div>
-				<div class="blue col mx-2">
-					Topveiling 2
-				</div>
-			</div>
-			<div class="row my-2">
-				<div class="blue col mx-2">
-					Veiling 1
-				</div>
-				<div class="blue col mx-2">
-					Veiling 2
-				</div>
-				<div class="blue col mx-2">
-					Veiling 3
-				</div>
-				<div class="blue col mx-2">
-					Veiling 5
-				</div>
-			</div>
-		</div>
-	</main>
+<main>
+    <div class="container">
+        <div class="row my-2">
+            <div class="auction-article-large white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        Titel:
+                    </div>
+                    <div class="col text-right">
+                        Prijs:
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/600/600" alt="EA">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        Beschrijving:
+                    </div>
+                    <div class="col text-right">
+                        <button>Details</button>
+                    </div>
+                </div>
+            </div>
+            <div class="auction-article-large white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        Titel:
+                    </div>
+                    <div class="col text-right">
+                        Prijs:
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/600/600" alt="EA">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        Beschrijving:
+                    </div>
+                    <div class="col text-right">
+                        <button>Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="white col mx-2">
+                Veiling 1
+            </div>
+            <div class="white col mx-2">
+                Veiling 2
+            </div>
+            <div class="white col mx-2">
+                Veiling 3
+            </div>
+            <div class="white col mx-2">
+                Veiling 4
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 <?php
 ?>
