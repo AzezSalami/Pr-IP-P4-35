62f0fcd8d314162949897a42f587d25d7f12245a
idries1710 <49497198+idries1710@users.noreply.github.com>
2019-05-06 16:45:45 +0200

    merge van homepage_layout en veilingen_op_homepage

--- homepage.php
+++ homepage.php
@@ -19,5 +19,176 @@
 <?php
 require "includes/header.php";
 ?>
+
+<main>
+    <div class="container">
+        <div class="row my-2">
+            <div class="auction-article-large white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/340/600/600" alt="EA">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button class="btn btn-details">Details</button>
+                    </div>
+                </div>
+            </div>
+            <div class="auction-article-large white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/350/600/600" alt="EA">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button class="btn btn-details">Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row my-2">
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/360/600/600" alt="EA">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button class="btn btn-details">Details</button>
+                    </div>
+                </div>
+            </div>
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/370/600/600" alt="EA">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button class="btn btn-details">Details</button>
+                    </div>
+                </div>
+            </div>
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/380/600/600" alt="EA">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button class="btn btn-details">Details</button>
+                    </div>
+                </div>
+            </div>
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/390/600/600" alt="EA">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <dt class="col"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button class="btn btn-details">Details</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
 </body>
 </html>
