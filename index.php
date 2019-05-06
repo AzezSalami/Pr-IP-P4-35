91e616914746620832a9e41793fd41b1e6c06d91
Denzel-Haverkamp <denzel.haverkamp@gmail.com>
2019-05-06 13:17:32 +0200

    opzet voor layout veilingen

--- index.php
+++ index.php
@@ -1,3 +1,44 @@
+<head>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+	      integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+	        integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+	        crossorigin="anonymous"></script>
+	<style>
+		.blue {
+			background-color: blue;
+			border: aqua 1px solid;
+		}
+	</style>
+</head>
+
+<body>
+	<main>
+		<div class="container">
+			<div class="row my-2">
+				<div class="blue col mx-2">
+					Topveiling 1
+				</div>
+				<div class="blue col mx-2">
+					Topveiling 2
+				</div>
+			</div>
+			<div class="row my-2">
+				<div class="blue col mx-2">
+					Veiling 1
+				</div>
+				<div class="blue col mx-2">
+					Veiling 2
+				</div>
+				<div class="blue col mx-2">
+					Veiling 3
+				</div>
+				<div class="blue col mx-2">
+					Veiling 4
+				</div>
+			</div>
+		</div>
+	</main>
+</body>
 <?php
-    
 ?>
