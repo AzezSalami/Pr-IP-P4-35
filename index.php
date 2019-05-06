56425e94f92a6469dbf9f4bc17731b58076db1a3
nielseenink <nielseenink@hotmail.com>
2019-05-06 15:56:50 +0200

    CSS: - Plaatjes schaalbaar gemaakt.
    PHP:
    
    - Nieuwe versie CDN.
    - Titel, prijs en beschrijving dikgedrukt gemaakt.
    - Test tekst toegevoegd.
    - Details knop verplaatst naar linksonder.

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
-            integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
-            crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous">
+    </script>
     <link rel="stylesheet" href="CSS/homepage.css" type="text/css">
 </head>
 
@@ -14,18 +14,24 @@
             <div class="auction-article-large white col mx-2">
                 <div class="row mt-3">
                     <div class="col">
-                        Titel:
+                        <dt class="col"> [Titel]</dt>
                     </div>
                     <div class="col text-right">
-                        Prijs:
+                        <dt class="col"> [Prijs]</dt>
                     </div>
                 </div>
                 <div class="row text-center">
-                    <img class="mx-auto my-2" src="https://picsum.photos/600/600" alt="EA">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/340/600/600" alt="EA">
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        Beschrijving:
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
                     </div>
                     <div class="col text-right">
                         <button>Details</button>
@@ -35,18 +41,24 @@
             <div class="auction-article-large white col mx-2">
                 <div class="row mt-3">
                     <div class="col">
-                        Titel:
+                        <dt class="col"> [Titel]</dt>
                     </div>
                     <div class="col text-right">
-                        Prijs:
+                        <dt class="col"> [Prijs]</dt>
                     </div>
                 </div>
                 <div class="row text-center">
-                    <img class="mx-auto my-2" src="https://picsum.photos/600/600" alt="EA">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/350/600/600" alt="EA">
                 </div>
                 <div class="row mb-3">
                     <div class="col">
-                        Beschrijving:
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
                     </div>
                     <div class="col text-right">
                         <button>Details</button>
@@ -55,17 +67,113 @@
             </div>
         </div>
         <div class="row my-2">
-            <div class="white col mx-2">
-                Veiling 1
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/360/600/600" alt="EA">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button>Details</button>
+                    </div>
+                </div>
             </div>
-            <div class="white col mx-2">
-                Veiling 2
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/370/600/600" alt="EA">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button>Details</button>
+                    </div>
+                </div>
             </div>
-            <div class="white col mx-2">
-                Veiling 3
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/380/600/600" alt="EA">
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <dt class="col text-left"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button>Details</button>
+                    </div>
+                </div>
             </div>
-            <div class="white col mx-2">
-                Veiling 4
+            <div class="auction-article-small white col mx-2">
+                <div class="row mt-3">
+                    <div class="col">
+                        <dt class="col"> [Titel]</dt>
+                    </div>
+                    <div class="col text-right">
+                        <dt class="col"> [Prijs]</dt>
+                    </div>
+                </div>
+                <div class="row text-center">
+                    <img class="mx-auto my-2" src="https://picsum.photos/id/390/600/600" alt="EA">
+                </div>
+                <div class="row mb-3">
+                    <div class="col">
+                        <dt class="col"> Beschrijving:</dt>
+                    </div>
+                    <div class="row mx-3">
+                        <dd class="col"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc molestie massa
+                            enim,
+                            eget pretium lorem accumsan id. Sed imperdiet, eros.
+                        </dd>
+                    </div>
+                    <div class="col text-right">
+                        <button>Details</button>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
