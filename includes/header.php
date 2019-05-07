c836161f94072cc3694b6b1e4b95fc0ab91059a6
AzezSalami <43267443+AzezSalami@users.noreply.github.com>
2019-05-07 09:48:23 +0200

    register popup update

--- includes/header.php
+++ includes/header.php
@@ -4,32 +4,34 @@
             <div class="col-lg-2">
                 <img src="images/ea.png" class="logo my-1 mx-auto d-block" alt="logo">
             </div>
-            <div class="col-lg-8 d-flex align-items-center mb-1" >
+            <div class="col-lg-8 d-flex align-items-center mb-1">
                 <div class="input-group">
                     <input type="text" class="form-control" placeholder="zoeken">
-                    <div class="input-group-append " >
+                    <div class="input-group-append ">
                         <button class="btn btn-outline-secondary bg-white" type="button" id="zoekknop">zoeken</button>
                     </div>
                 </div>
             </div>
             <div class="col-lg-2 my-auto text-center">
-                <button type="button" class="bg-lightblue btn mb-1" data-toggle="modal" data-target="#loginMenu"><i class="fas fa-user"></i> &nbsp; aanmelden</button>
+                <button type="button" class="bg-lightblue btn mb-1" data-toggle="modal" data-target="#loginMenu"><i
+                            class="fas fa-user"></i> &nbsp; aanmelden
+                </button>
             </div>
         </div>
     </div>
     <nav class="bg-yellow pt-3">
         <ul class="nav nav-tabs justify-content-center">
             <?php
-                $pages = ["home" => "homepage.php", "1" => "1.php", "2" => "2.php", "3" => "3.php", "4" => "4.php", "5" => "5.php", "6" => "6.php", "7" => "7.php"];
-                $html = "";
-                foreach ($pages as $page => $link) {
-                    $html .= "<li class='nav-item'><a class='nav-link";
-                    if (basename(htmlspecialchars($_SERVER["PHP_SELF"])) == basename($link)) {
-                        $html .= " currentpage";
-                    }
-                    $html .= "' href='$link'>$page</a></li>";
+            $pages = ["home" => "homepage.php", "1" => "1.php", "2" => "2.php", "3" => "3.php", "4" => "4.php", "5" => "5.php", "6" => "6.php", "7" => "7.php"];
+            $html = "";
+            foreach ($pages as $page => $link) {
+                $html .= "<li class='nav-item'><a class='nav-link";
+                if (basename(htmlspecialchars($_SERVER["PHP_SELF"])) == basename($link)) {
+                    $html .= " currentpage";
                 }
-                echo $html;
+                $html .= "' href='$link'>$page</a></li>";
+            }
+            echo $html;
             ?>
         </ul>
     </nav>
@@ -52,14 +54,12 @@
                         <div class="col">
                             <div class="form-group">
                                 <form method="POST" action="" name="registreren">
-                                    <label class="invisible" for="Username">Username<span
-                                                class="error">*</span></label>
+                                    <label class="invisible" for="Username">Username</label>
                                     <input class="form-control" placeholder="Username" type="text"
                                            name="Username"
                                            id="Username"
                                            maxlength="20" required><br>
-                                    <label class="invisible" for="Password">Password<span
-                                                class="error">*</span></label>
+                                    <label class="invisible" for="Password">Password</label>
                                     <input class="form-control" placeholder="Password"
                                            type="password" name="Password"
                                            id="Password"
@@ -94,70 +94,77 @@
 
                 <!-- Modal Header -->
                 <div class="modal-header">
+                    <h4 class="modal-title">Create an account</h4>
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                 </div>
 
                 <!-- Modal body -->
                 <div class="modal-body">
-                    <div class="">
-                        <div class="row">
-                            <div class="col">
-                                <h1 style="font-size:15px">Create an account</h1>
-                                <div class="form-row">
-                                    <form method="POST" action="" name="registreren">
-                                        <div class="row">
-                                            <div class="col">
-                                                <label class="invisible" for="Firstname">First name:<span
-                                                            class="error">*</span></label>
-                                                <input class="form-control" placeholder="First name" type="text"
-                                                       name="First name"
-                                                       id="Firstname" maxlength="20" required><br>
-                                            </div>
-                                            <div class="col">
-                                                <label class="invisible" for="Lastname">Last name<span
-                                                            class="error">*</span></label>
-                                                <input class="form-control" placeholder="Last name" type="text"
-                                                       name="Last name"
-                                                       id="Lastname"
-                                                       maxlength="20" required><br>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col">
-                                                <label class="invisible" for="Username">Username<span
-                                                            class="error">*</span></label>
-                                                <input class="form-control" placeholder="Username" type="text"
-                                                       name="regUsername"
-                                                       id="regUsername"
-                                                       maxlength="20" required><br>
-                                            </div>
-                                        </div>
-
-                                        <br>
-                                        <div class="row">
-                                            <div class="col">
-                                                <label class="invisible" for="Password">Password<span
-                                                            class="error">*</span></label>
-                                                <input class="form-control" placeholder="Password" type="password"
-                                                       name="regPassword"
-                                                       id="regPassword"
-                                                       maxlength="50" required><br>
-                                            </div>
-                                            <div class="col">
-                                                <label class="invisible" for="confirm">confirm<span
-                                                            class="error">*</span></label>
-                                                <input class="form-control" placeholder="confirm" type="password"
-                                                       name="confirm"
-                                                       id="confirm"
-                                                       maxlength="50" required><br>
-                                            </div>
-                                        </div>
-                                        <div class="row">
-                                            <input class="btn btn-primary" type="submit" name="next" value="next">
-                                        </div>
-                                    </form>
+                    <div class="container">
+                        <div class="form-row">
+                            <form method="POST" action="" name="registreren">
+                                <div class="row">
+                                    <div class="col">
+                                        <label class="invisible" for="Firstname">First name:</label>
+                                        <input class="form-control" placeholder="First name" type="text"
+                                               name="First name"
+                                               id="Firstname" maxlength="20" required><br>
+                                    </div>
+                                    <div class="col">
+                                        <label class="invisible" for="Lastname">Last name</label>
+                                        <input class="form-control" placeholder="Last name" type="text"
+                                               name="Last name"
+                                               id="Lastname"
+                                               maxlength="20" required><br>
+                                    </div>
                                 </div>
-                            </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <label class="invisible" for="Username">Username</label>
+                                        <input class="form-control" placeholder="Username" type="text"
+                                               name="regUsername"
+                                               id="regUsername"
+                                               maxlength="20" required><br>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <label class="invisible" for="address">Address</label>
+                                        <input class="form-control" placeholder="address" type="text"
+                                               name="address"
+                                               id="address"
+                                               maxlength="20" required><br>
+                                    </div>
+                                    <div class="col">
+                                        <label class="invisible" for="postcode">Postcode</label>
+                                        <input class="form-control" placeholder="postcode" type="text"
+                                               name="postcode"
+                                               id="postcode"
+                                               maxlength="20" required><br>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <label class="invisible" for="Password">Password</label>
+                                        <input class="form-control" placeholder="Password" type="password"
+                                               name="regPassword"
+                                               id="regPassword"
+                                               maxlength="50" required><br>
+                                    </div>
+                                    <div class="col">
+                                        <label class="invisible" for="confirm">confirm</label>
+                                        <input class="form-control" placeholder="confirm" type="password"
+                                               name="confirm"
+                                               id="confirm"
+                                               maxlength="50" required><br>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col">
+                                        <input class="btn bg-lightblue" type="submit" name="Register" value="Register">
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                 </div>
