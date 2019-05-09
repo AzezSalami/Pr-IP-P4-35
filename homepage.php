3de422712aa0c76db67a987217bc4f352600b43d
idries1710 <49497198+idries1710@users.noreply.github.com>
2019-05-09 11:32:01 +0200

    range filter gemaakt

--- homepage.php
+++ homepage.php
@@ -8,11 +8,10 @@
           integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
           crossorigin="anonymous">
 
-
-
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
           integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
           crossorigin="anonymous">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
@@ -33,6 +32,13 @@
     <meta name="apple-mobile-web-app-status-bar-style" content="#FFAD4F">
     <meta name="apple-mobile-web-app-status-bar-style" content="#FFAD4F">
     <link rel="stylesheet" href="CSS/homepage.css" type="text/css">
+
+
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <link rel="stylesheet" href="/resources/demos/style.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script src="JS/pricerange.js"></script>
 </head>
 <body class="bg-gray">
 <?php
@@ -43,8 +49,10 @@ require "includes/header.php";
 <main>
     <div class="row">
         <div class="col-lg-2">
-
-
+            <div class="container text-center my-2">
+                <input class="bg-gray text-center" type="text" id="amount" readonly style="border:0; color:#f6931f; font-weight:bold;">
+                <div class="my-2" id="slider-range"></div>
+            </div>
         </div>
         <div class="col-lg-8">
             <div class="container">
