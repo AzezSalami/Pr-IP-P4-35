e2c5a6567b364bad7569f4f9bb3c2453bbf8374b
nielseenink <nielseenink@hotmail.com>
2019-05-09 16:58:35 +0200

    Footer toegevoegd

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,7 @@
 <footer class="page-footer font-small blue">
 
     <!-- Copyright -->
-    <div class="footer-copyright text-center py-3">© 2019 Copyright:
-        <a href="https://icawiki.ml"> iConcepts</a>
+    <div class="footer-copyright text-center py-3">© 2019 Copyright: Groep 35
     </div>
     <!-- Copyright -->
 </footer>
