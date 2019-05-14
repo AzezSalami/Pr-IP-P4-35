18adff1fc1d60e8d71def4aa3db83bcaa6c275fd
nielseenink <nielseenink@hotmail.com>
2019-05-14 15:58:24 +0200

    Vragen en antwoorden ingevoerd op hulp pagina

--- hulp.php
+++ hulp.php
@@ -58,15 +58,15 @@ require "includes/header.php";
                         <div class="nav nav-pills faq-nav" id="faq-tabs" role="tablist" aria-orientation="vertical">
                             <a href="#tab1" class="nav-link active" data-toggle="pill" role="tab" aria-controls="tab1"
                                aria-selected="true">
-                                <i class="mdi mdi-help-circle"></i> Onderwerp 1
+                                <i class="mdi mdi-help-circle"></i> Registreren
                             </a>
                             <a href="#tab2" class="nav-link" data-toggle="pill" role="tab" aria-controls="tab2"
                                aria-selected="false">
-                                <i class="mdi mdi-account"></i> Onderwerp 2
+                                <i class="mdi mdi-account"></i> Inloggen
                             </a>
                             <a href="#tab3" class="nav-link" data-toggle="pill" role="tab" aria-controls="tab3"
                                aria-selected="false">
-                                <i class="mdi mdi-account-settings"></i> Onderwerp 3
+                                <i class="mdi mdi-account-settings"></i> Bieden
                             </a>
                         </div>
                     </div>
@@ -79,15 +79,19 @@ require "includes/header.php";
                                             <h5>
                                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                                         data-target="#accordion-tab-1-content-1" aria-expanded="false"
-                                                        aria-controls="accordion-tab-1-content-1">Vraag 1
+                                                        aria-controls="accordion-tab-1-content-1">Hoe kan ik een account
+                                                    aanmaken?
                                                 </button>
                                             </h5>
                                         </div>
                                         <div class="collapse show" id="accordion-tab-1-content-1"
                                              aria-labelledby="accordion-tab-1-heading-1" data-parent="#accordion-tab-1">
                                             <div class="card-body">
-                                                <p>Antwoord 1</p>
-                                                <p><strong>Toelichting: </strong>Voorbeeld</p>
+                                                <p>In de rechterbovenhoek van deze site kunt u een blauwe knop zien met
+                                                    de aanduiding account.
+                                                    Om uzelf te registeren moet u dan op de optie registeren kiezen.
+                                                    Nadat U het registratieproces
+                                                    voltooid heeft en geverifieerd hebt, heeft u een geldig account.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -96,15 +100,19 @@ require "includes/header.php";
                                             <h5>
                                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                                         data-target="#accordion-tab-1-content-2" aria-expanded="false"
-                                                        aria-controls="accordion-tab-1-content-2">Vraag 2
+                                                        aria-controls="accordion-tab-1-content-2">Ik krijg geen
+                                                    verificatiemail, wat nu?
                                                 </button>
                                             </h5>
                                         </div>
                                         <div class="collapse" id="accordion-tab-1-content-2"
                                              aria-labelledby="accordion-tab-1-heading-2" data-parent="#accordion-tab-1">
                                             <div class="card-body">
-                                                <p>Antwoord 2</p>
-                                                <p><strong>Toelichting 2: </strong>Voorbeeld</p>
+                                                <p>- U kunt proberen opnieuw een mail te laten sturen, klik dan op
+                                                    “stuur opnieuw”<br>
+                                                    - Check of u het goede e-mail adres heeft ingevoerd.</p>
+                                                <p><strong>Let op: </strong>Soms duur het even voordat de mail binnen
+                                                    is, wacht een paar minuten.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -117,15 +125,19 @@ require "includes/header.php";
                                             <h5>
                                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                                         data-target="#accordion-tab-2-content-1" aria-expanded="false"
-                                                        aria-controls="accordion-tab-2-content-1">Vraag 1
+                                                        aria-controls="accordion-tab-2-content-1">Hoe kan ik mijzelf
+                                                    inloggen?
                                                 </button>
                                             </h5>
                                         </div>
                                         <div class="collapse show" id="accordion-tab-2-content-1"
                                              aria-labelledby="accordion-tab-2-heading-1" data-parent="#accordion-tab-2">
                                             <div class="card-body">
-                                                <p>Antwoord 1</p>
-                                                <p><strong>Toelichting 1: </strong>Voorbeeld</p>
+                                                <p>In de rechterbovenhoek van deze site kunt u een blauwe knop zien met
+                                                    de aanduiding account. Om
+                                                    uzelf te registreren moet u dan de optie inloggen kiezen. Hier voert
+                                                    u het tijdens het registreren
+                                                    gekozen e-mail adres of gebruikersnaam en wachtwoord.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -134,15 +146,39 @@ require "includes/header.php";
                                             <h5>
                                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                                         data-target="#accordion-tab-2-content-2" aria-expanded="false"
-                                                        aria-controls="accordion-tab-2-content-2">Vraag 2
+                                                        aria-controls="accordion-tab-2-content-2">Het lukt mij niet in
+                                                    te loggen, wat nu?
                                                 </button>
                                             </h5>
                                         </div>
                                         <div class="collapse" id="accordion-tab-2-content-2"
                                              aria-labelledby="accordion-tab-2-heading-2" data-parent="#accordion-tab-2">
                                             <div class="card-body">
-                                                <p>Antwoord 2</p>
-                                                <p><strong>Toelichting 2: </strong>Voorbeeld</p>
+                                                <p>Probeer de volgende dingen:<br>
+                                                    - Controleer of uw gebruikersnaam correct is. ( dit is niet
+                                                    hoofdletter gevoelig)<br>
+                                                    - Controleer of uw wachtwoord goed is.<br>
+                                                    - Controleer of uw account al geverifieerd is.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="card">
+                                        <div class="card-header" id="accordion-tab-2-heading-3">
+                                            <h5>
+                                                <button class="btn btn-link" type="button" data-toggle="collapse"
+                                                        data-target="#accordion-tab-2-content-3" aria-expanded="false"
+                                                        aria-controls="accordion-tab-2-content-3">Ik ben mijn wachtwoord
+                                                    vergeten, wat nu?
+                                                </button>
+                                            </h5>
+                                        </div>
+                                        <div class="collapse" id="accordion-tab-2-content-3"
+                                             aria-labelledby="accordion-tab-2-heading-3" data-parent="#accordion-tab-2">
+                                            <div class="card-body">
+                                                <p>Als u uw wachtwoord vergeten bent, klik dan in het inlogvenster op
+                                                    “wachtwoord resetten”.
+                                                    U zal dan een mail ontvangen met daar in een link om uw wachtwoord
+                                                    opnieuw in te stellen.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -155,15 +191,18 @@ require "includes/header.php";
                                             <h5>
                                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                                         data-target="#accordion-tab-3-content-1" aria-expanded="false"
-                                                        aria-controls="accordion-tab-3-content-1">Vraag 1
+                                                        aria-controls="accordion-tab-3-content-1">Hoe kan ik een bod
+                                                    plaatsen op een rubriek?
                                                 </button>
                                             </h5>
                                         </div>
                                         <div class="collapse show" id="accordion-tab-3-content-1"
                                              aria-labelledby="accordion-tab-3-heading-1" data-parent="#accordion-tab-3">
                                             <div class="card-body">
-                                                <p>Antwoord 1</p>
-                                                <p><strong>Toelichting 1: </strong>Voorbeeld</p>
+                                                <p>Als u een rubriek bekijkt kunt u een invoer veld selecteren, waarin
+                                                    je het bedrag dat je wilt bieden
+                                                    invult. Om het bod definitief te maken drukt u op de knop
+                                                    “bieden”.</p>
                                             </div>
                                         </div>
                                     </div>
@@ -172,15 +211,22 @@ require "includes/header.php";
                                             <h5>
                                                 <button class="btn btn-link" type="button" data-toggle="collapse"
                                                         data-target="#accordion-tab-3-content-2" aria-expanded="false"
-                                                        aria-controls="accordion-tab-3-content-2">Vraag 2
+                                                        aria-controls="accordion-tab-3-content-2">Ik kan geen bod doen
+                                                    op een rubriek, hoe kan dit?
                                                 </button>
                                             </h5>
                                         </div>
                                         <div class="collapse" id="accordion-tab-3-content-2"
                                              aria-labelledby="accordion-tab-3-heading-2" data-parent="#accordion-tab-3">
                                             <div class="card-body">
-                                                <p>Antwoord 2</p>
-                                                <p><strong>Toelichting 2: </strong>Voorbeeld</p>
+                                                <p>Dit kan aan de volgende oorzaken liggen:<br>
+                                                    - De veiling is verlopen.<br>
+                                                    - Uw internetverbinding was slecht op moment van bieden.<br>
+                                                    - U bent niet ingelogd als verkoper of gebruiker.<br>
+                                                    o Weet u niet hoe u moet inloggen ? --> Kijk dan bij het kopje
+                                                    inloggen.<br>
+                                                    o Weet u niet hoe u moet registreren? --> Kijk dan bij het kopje
+                                                    registreren.</p>
                                             </div>
                                         </div>
                                     </div>
