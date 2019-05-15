89e602a707b279ab5ca90f22ee4d3805f098e727
idries1710 <49497198+idries1710@users.noreply.github.com>
2019-05-15 15:48:22 +0200

    alle paginas voor user story 16 af

--- wachtwoordresetten.php
+++ wachtwoordresetten.php
@@ -53,20 +53,19 @@ require "includes/header.php";
         </div>
         <div class="col-lg-8">
             <div class="text-dark">
-                <h1>Wachtwoord vergeten</h1>
-                <p>Weet je het wachtwoord niet meer?
-                    Vul hieronder je e-mailadres in.
-                    We sturen dan binnen enkele minuten een e-mail waarmee een nieuw wachtwoord kan worden
-                    aangemaakt.</p>
+                <h1>Wachtwoord resetten</h1>
+                <p>Vul hieronder het nieuwe wachtwoord in en klik op Verzenden.</p>
             </div>
-            <form class="text-dark">
+            <form>
                 <div class="form-label-group">
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"
-                           placeholder="e-mail">
-                    <label for="exampleInputEmail1">e-mail</label>
-
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Wachtwoord">
+                    <label for="exampleInputPassword1">Wachtwoord</label>
                 </div>
-                <button type="submit" class="btn bg-lightblue">Versturen</button>
+                <div class="form-label-group">
+                    <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Bevestig wachtwoord">
+                    <label for="exampleInputPassword2">Bevestig wachtwoord</label>
+                </div>
+                <button type="submit" class="btn bg-lightblue">Verzenden</button>
             </form>
         </div>
         <div class="col-lg-2">
