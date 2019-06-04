169764f5442f5af2fef964303c55cf0c8ed198bc
nielseenink <nielseenink@hotmail.com>
2019-06-04 13:57:01 +0200

    blokkeren veiling review

--- veilingen_details.php
+++ veilingen_details.php
@@ -1,45 +1,15 @@
+<head>
+    <?php include 'includes/head.html'; ?>
+    <link rel="stylesheet" href="CSS/general.css" type="text/css">
+    <link rel="stylesheet" href="CSS/veilingen-details.css" type="text/css">
+</head>
+
 <?php
 
 if (isset($_GET['auction'])) {
 
     echo '<!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>EenmaalAndermaal</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-          crossorigin="anonymous">
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
-          integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
-          crossorigin="anonymous">
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-    <script src="JS/sidenavscript.js"></script>
-    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="images/favicon-16x16.png">
-    <link rel="mask-icon" href="images/safari-pinned-tab.svg" color="#FFAD4F">
-    <meta name="msapplication-TileColor" content="#FFAD4F">
-    <meta name="theme-color" content="#FFAD4F">
-
-    <!-- Chrome, Firefox OS and Opera colored tabs-->
-    <meta name="theme-color" content="#FFAD4F">
-
-    <!-- Windows Phone -->
-    <meta name="msapplication-navbutton-color" content="#FFAD4F">
-
-    <!-- iOS Safari -->
-    <meta name="apple-mobile-web-app-status-bar-style" content="#FFAD4F">
-    <meta name="apple-mobile-web-app-status-bar-style" content="#FFAD4F">
-    <link rel="stylesheet" href="CSS/general.css" type="text/css">
-    <link rel="stylesheet" href="CSS/veilingen-details.css" type="text/css">
-
-</head>
 <body>';
 
     require "includes/header.php";
@@ -123,18 +93,17 @@ if (isset($_GET['auction'])) {
     if (isset($_POST['blockAuction'])) {
         $blockAuctionQuery = $pdo->prepare("UPDATE TBL_Auction SET is_blocked = 1 WHERE auction = ?");
         $blockAuctionQuery->execute(array($auctionid));
+        echo '<script>window.location.replace("index.php");</script>';
     }
 
     $emailQuery = $pdo->prepare("SELECT * FROM TBL_User WHERE [user] = ?");
-    $emailQuery -> execute(array($seller));
+    $emailQuery->execute(array($seller));
     $emailSeller = $emailQuery->fetch()['email'];
-
 
 
     echo "<main>
     <div class=\"row\"> 
         <div class=\"col-lg-2\">
-            <!---->
         </div>
         <div class=\"col-lg-8 text-dark veiling-details my-2 mx-3\">
             <div class=\"row my-3\">
@@ -142,7 +111,7 @@ if (isset($_GET['auction'])) {
                     <h2>$itemtitle</h2>
                 </div>
                 <div class=\"col-lg-3\">
-                    <h1 class='\prijs\'>€". ($itemprice > $itempricestart ? $itemprice : $itempricestart) . "</h1>
+                    <h1 class='\prijs\'>€" . ($itemprice > $itempricestart ? $itemprice : $itempricestart) . "</h1>
                 </div>
             </div>
             <div class=\"row\">
@@ -207,7 +176,10 @@ if (isset($_GET['auction'])) {
                             </div>
                         <p><span>Startdatum:</span> $startdate</p>
                         <p><span>Sluitdatum:</span> $enddate</p>
-                        <p><span>Minimale prijs:</span> €$itempricestart</p>            
+                        <p><span>Minimale prijs:</span> €$itempricestart</p> 
+                         <form method='POST'>
+            <br><button name='blockAuction' type='submit' class='btn'>Blokkeer veiling</button>
+</form>           
                     </div>
                 </div>
                 <div class=\"col-line\"></div>
@@ -239,7 +211,7 @@ if (isset($_GET['auction'])) {
     $html = "";
 
     while ($bid = $bidquery->fetch()) {
-        if($bid['user'] == null) {
+        if ($bid['user'] == null) {
             $html .= '<p class="bod button-left">&nbsp;&nbsp;<span class="verwijderd">Verwijderd:</span> &nbsp; €' . $bid['amount'] . '</p>';
         } else {
             $html .= '<p class="bod button-left">&nbsp;&nbsp;<span>' . $bid['user'] . ':</span> &nbsp; €' . $bid['amount'] . '</p>';
