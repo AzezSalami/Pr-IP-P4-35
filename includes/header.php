a44247fa796582dbe5de1fc960e6e28864b8276c
idries1710 <49497198+idries1710@users.noreply.github.com>
2019-06-07 14:44:32 +0200

    css modal

--- includes/header.php
+++ includes/header.php
@@ -326,7 +326,6 @@ logout();
                                 </div>
                                 <div class="col-lg">
                                     <div class="form-label-group">
-                                        <label class="invisible" for="confirm_password">bevestig wachtwoord</label>
                                         <input class="form-control" placeholder="bevestig wachtwoord" type="password"
                                                name="confirm_password"
                                                id="confirm_password"
